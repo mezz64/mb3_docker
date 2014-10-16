6b66ef95b99d307cdda679c98f8b01c8d1a7887e
mezz64 <mezz@johnmihalic.com>
2014-10-16 16:00:49 -0400

    Update media_browser.sh
    Add programdata command line switch

--- media_browser.sh
+++ media_browser.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 umask 000
 
+# Start MediaBrowser Server
 cd /opt/MediaBrowser
-exec /sbin/setuser nobody mono /opt/MediaBrowser/MediaBrowser.Server.Mono.exe
+exec /sbin/setuser nobody mono /opt/MediaBrowser/MediaBrowser.Server.Mono.exe -programdata /config
